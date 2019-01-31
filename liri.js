3e454830dced220347b6ad4d86e2c964f597f097
Nathan <nathanlserrano@gmail.com>
2019-01-30 20:16:34 -0800

    Created code for initial commands in liri.js

--- liri.js
+++ liri.js
@@ -2,3 +2,19 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+if (process.argv[2] === "concert-this") {
+    // search bands in town api
+}
+else if (process.argv[2] === "spotify-this-song") {
+    // 
+}
+else if (process.argv[2] === "movie-this") {
+    // 
+}
+else if (process.argv[2] === "do-what-it-says") {
+    // 
+}
+else {
+    console.log("Incorrect command. Please try again.")
+}
