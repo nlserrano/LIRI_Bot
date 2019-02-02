c96bb29565100784c110c0b76d45ff19f13f93b6
Nathan <nathanlserrano@gmail.com>
2019-02-01 20:21:44 -0800

    Updated required dependencies in liri.js

--- liri.js
+++ liri.js
@@ -1,10 +1,33 @@
 require("dotenv").config();
+var moment = require("moment");
+var request = require("request");
+var moment = require("moment");
 
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+
+// var keys = require("./keys.js");
+// var spotify = new Spotify(keys.spotify);
+
+var userInput = process.argv[3];
 
 if (process.argv[2] === "concert-this") {
+    var artist = process.argv.slice(3).join(" ")
+    console.log(artist);
+   
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    request(queryURL, function (error, response, body) {
+        if (error) console.log(error);
+        var result  =  JSON.parse(body)[0];
+        console.log("Venue name " + result.venue.name);
+        console.log("Venue location " + result.venue.city);
+        console.log("Date of Event " +  moment(result.datetime).format("MM/DD/YYYY"));
+       
+
+
+    });
+      
     // search bands in town api
+    // https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp
 }
 else if (process.argv[2] === "spotify-this-song") {
     // 
