32a196afa93b826da8f02add361d719db6a5befe
Nathan <nathanlserrano@gmail.com>
2019-02-01 20:48:10 -0800

    Wrote out command line functions

--- liri.js
+++ liri.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
 var moment = require("moment");
 var request = require("request");
-var moment = require("moment");
+var fs = require("fs");
 
 
 // var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
+// var Spotify = new Spotify(keys.spotify);
 
-var userInput = process.argv[3];
+// var spotify = new spotify({
+//     id: MSMediaKeySession.spotify.id,
+//     secret: MSMediaKeySession.spotify.secret
+// });
+
 
 if (process.argv[2] === "concert-this") {
     var artist = process.argv.slice(3).join(" ")
@@ -21,6 +25,7 @@ if (process.argv[2] === "concert-this") {
         console.log("Venue name " + result.venue.name);
         console.log("Venue location " + result.venue.city);
         console.log("Date of Event " +  moment(result.datetime).format("MM/DD/YYYY"));
+        console.log(result);
        
 
 
@@ -30,13 +35,61 @@ if (process.argv[2] === "concert-this") {
     // https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp
 }
 else if (process.argv[2] === "spotify-this-song") {
-    // 
+    var songName = process.argv.slice(3).join(" ");
+    if (songName == undefined) {
+        songName = "Song is not defined";
+    }
+    spotify.search({ type: 'track', query: songName, limit: 10 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var tableArray = [];
+        for (var i = 0; i < data.tracks.items.length; i++) {
+            var result = {
+                artist: data.tracks.items[i].album.artists[0].name,
+                song_name: data.tracks.items[i].name,
+                album_name: data.tracks.items[i].album.name,
+                preview_url: data.tracks.items[i].preview_url
+
+                
+            }
+            tableArray.push(result);
+        }
+        var table = cTable.getTable(tableArray);
+        console.log(table);
+
+
+    });
 }
 else if (process.argv[2] === "movie-this") {
-    // 
+    var movieName = process.argv.slice(3).join(" ");
+
+    if (movieName == undefined) {
+        movieName = "Movie is undefined err";
+    }
+
+    request('http://www.omdbapi.com/?i=tt3896198&apikey=55e8eecb&t=' + process.argv[3], function (error, response, body) {
+
+        var result = JSON.parse(body);
+        console.log("Title :" + result.Title);
+        console.log("Year :" + result.Released);
+        console.log("IMDB Rating :" + result.imdbRating);
+        console.log("Rotten Tomatoes :" + result.Ratings[1].Value);
+        console.log("Country :" + result.Country);
+        console.log("Language :" + result.Language);
+        console.log("Movie Plot :" + result.Plot);
+        console.log("Actors :" + result.Actors);
+
+    });
 }
 else if (process.argv[2] === "do-what-it-says") {
-    // 
+    fs.readFile('random.txt',"utf8", (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        console.log(data)
+    });
 }
 else {
     console.log("Incorrect command. Please try again.")
